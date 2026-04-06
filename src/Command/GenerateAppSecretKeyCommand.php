0bc71b361dbda7c5511386b43a757680b2c18b08
Cheikh Dieye <email>
2026-04-07 01:15:02 +0200

    ajout de commande: création permettant de créer la commande .env.dev.local et initialisation de APP_SECRET.

---
+++ src/Command/GenerateAppSecretKeyCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:generate-local-secret-key',
+    description: 'Creates the .env.dev.local file and initializes APP_SECRET key',
+)]
+class GenerateAppSecretKeyCommand extends Command
+{
+
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        
+        $envFile = ".env.dev.local";
+
+
+        if ( file_exists($envFile)) {
+        $io->error("The {$envFile} file already exists");    
+        return Command::FAILURE; 
+        }
+
+        $keySecret = bin2hex(random_bytes(16));
+
+        file_put_contents($envFile, "APP_SECRET={$keySecret}");
+
+        $io->success("The .env.dev.local file created and APP_SECRET key initialized.");
+
+
+        return Command::SUCCESS;
+    }
+}
