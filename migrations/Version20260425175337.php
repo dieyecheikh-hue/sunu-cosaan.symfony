7bea6fcebaf59807feae3c77e9330aa5a223ca48
Cheikh Dieye <email>
2026-04-26 06:56:13 +0200

    Mise en place des articles

---
+++ migrations/Version20260425175337.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260425175337 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_5A8A6C8DC53D045F ON post');
+        $this->addSql('ALTER TABLE post CHANGE image image VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE post CHANGE image image VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_5A8A6C8DC53D045F ON post (image)');
+    }
+}
